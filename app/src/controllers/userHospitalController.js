96f7a893ddf9dd62a7444b055c3d03977315fa64
GuilhermeDelfino <guilherme.narciso@sptech.school>
2022-09-25 19:50:57 -0300

    :construction: /database/config.js change for more clean code
    Mudei tambem algumas mudancas no insert de hospital e de usuario e o usuario agora vai poder inserir o hospital e o user ao mesmo tempo

--- app/src/controllers/userHospitalController.js
+++ app/src/controllers/userHospitalController.js
@@ -1,35 +1,36 @@
-var usuarioModel = require("../models/usuarioModel");
 var userHospitalModel = require("../models/userHospitalModel");
 
 /**
- * @param {Request} req 
- * @param {Response} res 
+ * @param {Request} req
+ * @param {Response} res
  */
-
-async function insertUsuario(req,res){
-    //res.send("Estou na controller do hospital");
-    // res.send(hospitalModel.insert());
-    console.log(`req.body`, req.body)
-
-    if(Object.values(req.body).length !== 3){
-        const msg = "Campos invalidos, valide no arquivo hospital controller quais os camposq que essa requisicao pede." ;
-        res
-        .json({
-            msg: msg
-        })
-        .status(404)
-    }else{
-        const parametros = {
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password
-        }
-        res
-        .json(await userHospitalModel.insertUsuario(parametros))
-        .status(201)
-    }
+async function insertUsuario(req, res) {
+  //   console.log(`req.body`, req.body);
+  if (Object.values(req.body).length !== 5) {
+    const msg =
+      "Campos invalidos, valide no arquivo userHospitalController quais os campos que essa requisicao pede. (funcão insertUsuario de userHospitalController.js)";
+    res
+      .json({
+        data: null,
+        msg: msg,
+        status: 404,
+      })
+      .status(404);
+  } else {
+    const parametros = {
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      manager: req.body.manager || null,
+      hospital: req.body.hospital || null,
+    };
+    const userHospitalResult = await userHospitalModel.insertUsuario(
+      parametros
+    );
+    res.json(userHospitalResult).status(userHospitalResult.status);
+  }
 }
 
 module.exports = {
-    insertUsuario
-}
+  insertUsuario,
+};
--- app/src/models/userHospitalModel.js
+++ app/src/models/userHospitalModel.js
@@ -1,25 +1,20 @@
-var database = require("../database/config")
-
-
-
+var database = require("../database/config");
 
 async function insertUsuario({
-    name,
-    email,
-    password
-}){
+  name = "",
+  email = "",
+  password = "",
+  manager = null,
+  hospital = null,
+}) {
+  // query vai ser nosso comando sql -> para inserir -> insert into
 
-    // query vai ser nosso comando sql -> para inserir -> insert into
-    
-    const query = 
-    `INSERT INTO UserHospital (name, email, password) VALUES
-    ('${name}', '${email}', '${password}')`;
+  const query = `INSERT INTO UserHospital (name, email, password, idManager, fkHospital) VALUES
+    ('${name}', '${email}', '${password}', ${manager}, ${hospital})`;
 
-    return await database.insertUsuario(query);
-
+  return await database.insert(query);
 }
 
-
 module.exports = {
-    insertUsuario
+  insertUsuario,
 };
