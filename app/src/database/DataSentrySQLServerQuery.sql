536262c7e1179b6a445f61848a7d4dc86c50f442
GuilhermeDelfino <guilherme.narciso@sptech.school>
2022-09-24 23:03:07 -0300

    :card_file_box: Script MSSQL SERVER

--- app/src/database/DataSentrySQLServerQuery.sql
+++ app/src/database/DataSentrySQLServerQuery.sql
@@ -1,31 +1,103 @@
--- DROP DATABASE dataSentry;
-CREATE DATABASE dataSentry;
-USE dataSentry;
-
-CREATE TABLE Hospital (
-idHospital INT PRIMARY KEY identity(1,1),
-cnpj CHAR(14),
-cep CHAR(8),
-numberAdress varchar(6),
-unit VARCHAR(10),
-fantasyName VARCHAR(45),
-corporateName VARCHAR(45),
-complement VARCHAR(45),
-createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
-updateAt DATETIME DEFAULT CURRENT_TIMESTAMP
+CREATE DATABASE datasentry;
+USE datasentry;
+ 
+CREATE TABLE Hospital(
+	_idHospital INT PRIMARY KEY IDENTITY(1,1),
+	cnpj CHAR(14),
+	cep CHAR(8),
+	numberAddress SMALLINT,
+	complement VARCHAR(25),
+	fantasyName VARCHAR(50),
+	corporateName VARCHAR(50),
+	createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+	updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP 
 );
 
-CREATE TABLE UserHospital (
-idUser INT PRIMARY KEY identity(1,1),
-name VARCHAR(45),
-email VARCHAR(45),
-password VARCHAR(150), -- isso aqui precisar dps estar com encriptacao md5 ou EAS
-fkHospital INT,
-FOREIGN KEY (fkHospital) REFERENCES Hospital(idHospital),
-idManager INT,
-FOREIGN KEY (idManager) REFERENCES UserHospital(idUser),
--- recursiva ou seja, quando o hospital criar um user analista novo, este vai herdar o ID do user hospital 
-createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
-updateAt DATETIME DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE contactPhoneHospital(
+	_idContactPhoneHospital INT IDENTITY(1,1),
+	contactPhone CHAR(13),
+	fkHospital INT NOT NULL,
+	FOREIGN KEY (fkHospital) REFERENCES Hospital(_idHospital),
+	createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+	updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY(_idContactPhoneHospital, fkHospital)
 );
 
+CREATE TABLE UserHospital(
+	_idUserHospital INT PRIMARY KEY IDENTITY(1,1),
+	name VARCHAR(100),
+	email VARCHAR(100),
+	password VARCHAR(100),
+	contactPhone CHAR(13),
+	fkHospital INT NOT NULL,
+	FOREIGN KEY (fkHospital) REFERENCES Hospital(_idHospital),
+	fkManager INT,
+	FOREIGN KEY (fkManager) REFERENCES UserHospital(_idUserHospital),
+	createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+	updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP 
+);
+
+CREATE TABLE Server(
+	_serialServer VARCHAR(30) PRIMARY KEY, -- senão pegar serial vamos pegar outro dado único do PC
+	isActive CHAR(1),
+	fkHospital INT NOT NULL,
+	FOREIGN KEY (fkHospital) REFERENCES Hospital(_idHospital),
+	createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+	updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP 
+);
+
+CREATE TABLE UserHasServer(
+	_idUserHasServer INT IDENTITY(1,1),
+	isActive CHAR(1),
+	fkServer VARCHAR(30) NOT NULL,
+	FOREIGN KEY (fkServer) REFERENCES Server(_serialServer),
+	fkUserHospital INT NOT NULL,
+	FOREIGN KEY (fkUserHospital) REFERENCES UserHospital(_idUserHospital),
+	createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+	updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (_idUserHasServer, fkServer, fkUserHospital)
+);
+
+CREATE TABLE Process(
+	_idProcess INT PRIMARY KEY IDENTITY(1,1), -- talvez vire o PID
+	name VARCHAR(25),
+	createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+	updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP 
+);
+
+CREATE TABLE ComponentType( 
+	_idComponentType INT PRIMARY KEY IDENTITY(1,1),
+	description VARCHAR(25), 
+	measuramentUnit VARCHAR(5),
+	createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+	updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP 
+);
+
+CREATE TABLE ComponentServer(
+	_idComponentServer INT IDENTITY(1,1),
+	serial VARCHAR(30),
+	model VARCHAR(25),
+	yearManufatured CHAR(4),
+	brand VARCHAR(25),
+	fkServer VARCHAR(30) NOT NULL,
+	FOREIGN KEY (fkServer) REFERENCES Server(_serialServer),
+	fkComponentType INT NOT NULL,
+	FOREIGN KEY (fkComponentType) REFERENCES ComponentType(_idComponentType),
+	createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+	updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (_idComponentServer, fkComponentType)
+);
+
+CREATE TABLE LogComponentPerProcess(
+	_idLogComponentPerProcess INT IDENTITY(1,1),
+	usageComponent decimal(10,2),
+	createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+	updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+	fkProcess INT,
+	FOREIGN KEY (fkProcess) REFERENCES Process(_idProcess),
+	fkComponentType INT,
+	fkComponentServer INT,
+	FOREIGN KEY (fkComponentServer, fkComponentType)
+	REFERENCES ComponentServer(_idComponentServer, fkComponentType),
+	PRIMARY KEY (_idLogComponentPerProcess, fkComponentServer, fkComponentType, fkProcess)
+);
