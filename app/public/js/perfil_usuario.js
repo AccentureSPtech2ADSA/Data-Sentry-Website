8f993418496863833d1c5825d1375eeaaeb09b0b
eduardoSilva96 <eduardo.rego@gmail.com>
2022-09-24 22:43:15 -0300

    Adicionado A biblioteca do JQUERY e o plugin do jQuery Mask

--- app/public/js/perfil_usuario.js
+++ app/public/js/perfil_usuario.js
@@ -104,3 +104,10 @@ function onoff(){
     document.getElementById("id_input8").value = "Brasil";
     document.getElementById("id_input9").value = "***************";
   }
+
+$(document).ready(function(){
+    $('#inp_cep').mask('00.000-000');
+    $('#inp_telefone').mask('(00) 00000-0000');
+    $('#inp_cpf').mask('000.000.000-00', {reverse: true});
+    $('#inp_data').mask('00/00/0000', {placeholder: '__/__/____'})
+});
