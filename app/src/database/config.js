d864195f324913e66b75741e7d5cca77a1425eaa
GuilhermeDelfino <guilherme.narciso@sptech.school>
2022-09-26 00:41:11 -0300

    :construction: Working at create hospital and user together

--- app/src/database/config.js
+++ app/src/database/config.js
@@ -1,5 +1,6 @@
 const mysql = require("mysql2");
 const sql = require("mssql");
+
 const AMBIENTE = process.env.AMBIENTE_PROCESSO;
 const NOME_AMBIENTE_PRODUCAO = "PROD";
 const NOME_AMBIENTE_DESENVOLVIMENTO = "DEV";
@@ -30,106 +31,71 @@ var mySqlConfig = {
   user: "root",
   password: "datasentry",
 };
+const msgResponseError = `Houve algum erro executar a query.`;
+const msgResponseOk = `Query executada com sucesso.`;
 
-// as quatros funcoes abaixos tem que ter a base para executar nossos scripts de banco de dados
-// ou seja, no sql vamos receber como parametro uma string com uma query sql select
-// vamos ter que retornar de forma IGUAL os dados
-// ou seja, se no mysql retornou {nome: 'azul'} no azure tambem tem que retornar a mesma coisa
-// digo isso agora porque por padrao eles vem com formatos de retorno diferente
-// se baseie na funcao executar abaixo, vamos fazer de maneira diferente e melhor!
-function insertMysql(querySQL) {
+let response = {
+  status: 500,
+  msg: msgResponseError,
+};
+
+function executeMysql(querySQL) {
   return new Promise((resolve) => {
-    const msgResponse = `Houve algum erro ao inserir no banco de dados \nQuery: ${querySQL}`;
-    const response = {
-      status: 500,
-      msg: msgResponse,
-    };
     const conexaoMysql = mysql.createConnection(mySqlConfig);
     conexaoMysql.connect();
     conexaoMysql.query(querySQL, (err, result) => {
       if (err) {
-        response.data = err;
-      } else {
-        const msgResponse = `Inserido com sucesso no ID = ${result.insertId} `;
-        response.data = result;
-        response.msg = msgResponse;
-        response.status = 201;
+        console.log(err);
+        err.status = 500;
+        err.msg = msgResponseError;
+        resolve(err);
       }
-      conexaoMysql.end();
-
+      response.msg = msgResponseOk;
+      response.status = 200;
+      response.data = result;
       resolve(response);
+      conexaoMysql.end();
     });
   });
 }
-function insertSqlServer(querySQL) {}
+function executeSqlServer(querySQL) {
+  return new Promise(async (resolve) => {
+    try {
+      await sql.connect(sqlServerConfig);
+      const resultMssql = await sql.query(querySQL);
+      response.status = 201;
+      response.msg = msgResponseOk;
+      response.data = resultMssql.recordsets;
+      resolve(response);
+    } catch (error) {
+      response.data = error;
+      // console.dir(response);
+      resolve(response);
+    }
+  });
+}
 /**
- * @description Vai inserir de acordo com o ambiente de desenvolvimento, ou sql server ou mysql
+ * @description Vai executar a query de acordo com o ambiente de desenvolvimento, ou sql server ou mysql
  * @param {string} querySQL Query para insert ex: INSERT INTO User VALUES(1);
  * @returns {{data: mysql.ResultSetHeader | sql.IRecordSet, msg: string, status: number}}
  */
-function insert(querySQL) {
+function execute(querySQL) {
   return new Promise(async (resolve) => {
+    let result = {};
     if (isAmbienteDesenvolvimento) {
-      const resultInsertMysql = await insertMysql(querySQL);
-      resolve(resultInsertMysql);
+      result = await executeMysql(querySQL);
     } else if (isAmbienteProducao) {
-      resolve(insertSqlServer(querySQL));
+      result = await executeSqlServer(querySQL);
     } else {
-      console.log("process.env.AMBIENTE incorreto");
+      console.dir("process.env.AMBIENTE incorreto");
     }
+    resolve(result);
   })
     .then((res) => res)
     .catch((err) => err);
 }
-
-function executar(instrucao) {
-  // VERIFICA A VARIÁVEL DE AMBIENTE SETADA EM app.js
-  if (process.env.AMBIENTE_PROCESSO == "producao") {
-    return new Promise(function (resolve, reject) {
-      sql
-        .connect(sqlServerConfig)
-        .then(function () {
-          return sql.query(instrucao);
-        })
-        .then(function (resultados) {
-          console.log(resultados);
-          resolve(resultados.recordset);
-        })
-        .catch(function (erro) {
-          reject(erro);
-          console.log("ERRO: ", erro);
-        });
-      sql.on("error", function (erro) {
-        return "ERRO NO SQL SERVER (Azure): ", erro;
-      });
-    });
-  } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
-    return new Promise(function (resolve, reject) {
-      var conexao = mysql.createConnection(mySqlConfig);
-      conexao.connect();
-      conexao.query(instrucao, function (erro, resultados) {
-        conexao.end();
-        if (erro) {
-          reject(erro);
-        }
-        console.log(resultados);
-        resolve(resultados);
-      });
-      conexao.on("error", function (erro) {
-        return "ERRO NO MySQL WORKBENCH (Local): ", erro.sqlMessage;
-      });
-    });
-  } else {
-    return new Promise(function (resolve, reject) {
-      console.log(
-        "\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n"
-      );
-      reject("AMBIENTE NÃO CONFIGURADO EM app.js");
-    });
-  }
-}
-
 module.exports = {
-  executar,
-  insert,
+  execute,
+  isAmbienteDesenvolvimento,
+  isAmbienteProducao,
 };
